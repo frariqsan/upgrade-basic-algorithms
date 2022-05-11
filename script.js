7f275cdb7b7cf253117536aa9df6e180f1762b63
frariqsan <frariqsan@gmail.com>
2022-05-11 22:04:05 +0200

    Iteration#3

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Iteracion #1 Variables
 // 1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
 let myFavoriteHero = "Hulk";
 // 1.2 Crea una variable llamada x, asigna el valor 50 a ella.
@@ -7,3 +8,17 @@ let h = 5;
 let y = 10;
 // 1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.
 let z = h + y;
+//--------------------------------------------------------------------------------------//
+//Iteración #3 Operadores
+// 1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+    let multiplication = 10 * 5; 
+    alert(multiplication);
+// 1.2 Divide 10 por 2 y muestra el resultado en un alert.
+    let divide = 10/2;
+    alert(divide);
+// 1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+    let rest = 15%9;
+    alert(rest);
+// 1.4 Usa el correcto operador de asignación que resultará en x = 15, teniendo dos variables y = 10 y z = 5.
+
+// 1.5 Usa el correcto operador de asignación que resultará en x = 50, teniendo dos variables y = 10 y z = 5.
